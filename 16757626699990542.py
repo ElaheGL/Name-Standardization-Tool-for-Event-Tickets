b1622926929ffc297357c93cef8ce8790fa0581b
ElaheGl <golielahe8@gmail.com>
2023-11-12 23:36:20 +0330

    Upload Simple Name-Standardization-Tool-for-Event-Tickets project on Github

---
+++ 16757626699990542.py
@@ -0,0 +1,10 @@
+enter_num = 0
+lis_num = []
+for i in range(10):
+    enter_num = str(input())
+    y = enter_num.lower()
+    x= y.capitalize()
+    lis_num.append(x)
+   
+for f in lis_num:
+    print(f)
